8cfd64a9e1e9d499b309ccb92df98e22f087b9f3
takattty <r9r_celtics_t9t@icloud.com>
2021-08-19 20:10:28 +0900

    modify admin/movies files

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,7 +1,5 @@
 class Admin::MoviesController < ApplicationController
   def index
-    movies = [{"id" => 1, "name" => "hoge", "year" => 1997, "is_showing" => 1, "description" => "hogehoge", "image_url" => "https://techbowl.co.jp/_nuxt/img/1.png", "created_at" => "1008", "updated_at" => "1009" }]
-    # @movies = Movie.all
-    @movies = movies
+    @movies = Movie.all
   end
 end
